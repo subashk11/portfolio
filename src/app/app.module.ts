73b5bec325049bbbea357e0cd1207c3a51d47236
subashk11 <subashk1153@gmail.com>
2024-07-10 22:16:20 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
+import { WelcomePageComponent } from './container/welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
