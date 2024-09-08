0dcb59533329c5a8dd89ae1803dcfa9d2a5dba09
subashk11 <subashk1153@gmail.com>
2024-09-08 11:36:26 +0530

    minor code changes

--- src/app/container/projects-page/projects-page.component.ts
+++ src/app/container/projects-page/projects-page.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects-page',
   templateUrl: './projects-page.component.html',
   styleUrls: ['./projects-page.component.css']
 })
-export class ProjectsPageComponent {
-
+export class ProjectsPageComponent{
+  
 }
