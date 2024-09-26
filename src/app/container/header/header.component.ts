5a086dcfd4e210746ffb0dc15675b51463b20d93
subashk11 <subashk1153@gmail.com>
2024-09-27 00:03:27 +0530

    Made welcome, header and about page as responsive

--- src/app/container/header/header.component.ts
+++ src/app/container/header/header.component.ts
@@ -7,32 +7,43 @@ import { ContainerService } from '../container.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isMobileMenuOpen: boolean;
   constructor(public containerService:ContainerService){}
 
   //ENABLE HOME PAGE LAYOUT
   openHome(){
     this.containerService.updateRefOfStage.next('welcome');
     this.containerService.currentPage = 0;
+    this.toggleMobileMenu()
   }
 
   //OPEN ABOUT PAGE
   openAbout(){
     this.containerService.updateRefOfStage.next('about');
     this.containerService.currentPage = 1;
+    this.toggleMobileMenu()
   }
 
   openSkills(){
     this.containerService.currentPage = 2;
     this.containerService.updateRefOfStage.next('skills');
+    this.toggleMobileMenu()
   }
 
   openProjects(){
     this.containerService.currentPage = 3;
     this.containerService.updateRefOfStage.next('projects');
+    this.toggleMobileMenu()
+
   }
 
   openContactInfo(){
     this.containerService.currentPage = 4;
     this.containerService.updateRefOfStage.next('contactInfo')
+    this.toggleMobileMenu()
+  }
+
+  toggleMobileMenu(){
+    this.isMobileMenuOpen = !this.isMobileMenuOpen
   }
 }
